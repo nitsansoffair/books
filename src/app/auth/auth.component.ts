522ac325d31a056ef0a073b47ed0babedac5fad8
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-22 02:43:23 +0200

    Add authentication - component, service

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+import { AuthResponseData, AuthService } from './auth.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+
+    if (this.isLoginMode) {
+      authObs = this.authService.login(email, password);
+    } else {
+      authObs = this.authService.signup(email, password);
+    }
+
+    authObs.subscribe((responseData) => {
+      this.isLoading = false;
+      this.router.navigate(['/books']);
+    }, (errorMessage) => {
+      this.error = errorMessage;
+      this.isLoading = false;
+    });
+
+    form.reset();
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+}
